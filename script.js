205a057288deddff127e0d73ebc94aa767d0b8d7
miranda3094 <jekamiranda30@gmail.com>
2025-06-10 19:00:30 -0600

    se termina script y se pasa a pruebas

--- script.js
+++ script.js
@@ -46,37 +46,45 @@ document.addEventListener('DOMContentLoaded',()=>{
             mostrarError(errorNombre,nombreInput,'El nombre es requerido');
             formValido = false;
         }
-        //validacion para correo electronico
+        //Validacion para correo electronico
+
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (emailInput.value.trim() === '') {
-            mostrarError(errorEmail, emailInput,'El correo es requerido');
+        if (emailInput.value.trim() === ''){
+            mostrarError(errorEmail,emailInput,'El correo es requerido');
             formValido = false;
-        } else if (emailPattern.test(emailInput.value.trim())){
+        }else if (!emailPattern.test(emailInput.value.trim())){
             mostrarError(errorEmail,emailInput,'Ingrese un correo valido');
             formValido = false;
         }
-        //validacion de la contraseña
-        if (passwordInput.value.trim() === '') {
-            mostrarError(errorPassword, passwordInput,'La contraseña es requerida');
+        //Validacion de la contraseña
+        if (passwordInput.value.trim() === ''){
+            mostrarError(errorPassword,passwordInput,'la contraseña es requerida');
             formValido = false;
-        } else if (passwordInput.value.trim().length < 6) {
-            mostrarError(errorPassword,passwordInput,'La contraseña debe de ser mayor a 6 caracteres');
+
+        }else if (passwordInput.value.trim().length <6){
+            mostrarError(errorPassword,passwordInput,'La contreseña debe de ser mayor a 6');
             formValido = false;
         }
+
         //Validacion de la confirmacion de la contraseña
-        if (confirmPasswordInput,value.trim() === '') {
+
+        if (confirmPasswordInput.value.trim() === ''){
             mostrarError(errorConfirmPassword,confirmPasswordInput,'Debe de reingresar la contraseña');
             formValido = false;
-        } else if (confirmPasswordInput.value !== passwordInput.value) {
-            mostrarError(errorConfirmPassword,confirmPasswordInput,'Las contraseñas deber ser iguales');
-            formValido = value;
+        }else if (confirmPasswordInput.value !== passwordInput.value){
+            mostrarError(errorConfirmPassword,confirmPasswordInput,'Las contraseñas deben ser iguales');
+            formValido = false;
         }
+
         //Si el formulario es valido, mostrar el mensaje de exito y resetear el formulario
+
         if (formValido) {
             successMessage.style.display = 'block';
-            registroForm.reset(); //limpia todos los campos del formulario
+            registroForm.reset(); // Limpia todos los campos del formulario
             console.log('Formulario enviado correctamente !');
+
         }
+
     });
 
 });
